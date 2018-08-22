3b0846401bafc55886ba71c05aa81010a162beb7
Ramadani <email.ramadani@gmail.com>
2018-08-22 16:52:08 +0700

    add Graceful Shutdown

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -1,12 +1,17 @@
 package bootstrap
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ramadani/go-api-skeleton/routes"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 type App struct {
@@ -23,7 +28,24 @@ func (app App) InitRoutes() {
 }
 
 func (app App) Run(port int) {
-	app.e.Logger.Fatal(app.e.Start(fmt.Sprintf(":%d", port)))
+	app.e.Logger.SetLevel(log.INFO)
+
+	go func() {
+		if err := app.e.Start(fmt.Sprintf(":%d", port)); err != nil {
+			app.e.Logger.Info("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 10 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := app.e.Shutdown(ctx); err != nil {
+		app.e.Logger.Fatal(err)
+	}
 }
 
 func New(e *echo.Echo) *App {
