9f7248608450cb45a09ba5faeff80d4d45a0e39d
Ramadani <email.ramadani@gmail.com>
2018-08-31 11:04:59 +0700

    implement resource for responses

--- app/todo/usecase.go
+++ app/todo/usecase.go
@@ -2,14 +2,14 @@ package todo
 
 import (
 	"github.com/ramadani/go-api-skeleton/app/commons/response"
-	"github.com/ramadani/go-api-skeleton/app/todo/data"
+	"github.com/ramadani/go-api-skeleton/app/todo/resource"
 )
 
 // UseCase for todo's logic
 type UseCase interface {
-	All() []data.Todo
-	Create(title, body string) data.Todo
-	Find(id uint) data.Todo
-	Update(title, body string, id uint) data.Todo
+	All() []resource.Todo
+	Create(title, body string) resource.Todo
+	Find(id uint) resource.Todo
+	Update(title, body string, id uint) resource.Todo
 	Delete(id uint) response.Message
 }
--- app/todo/usecase/todo_usecase.go
+++ app/todo/usecase/todo_usecase.go
@@ -4,27 +4,35 @@ import (
 	"github.com/ramadani/go-api-skeleton/app/commons/response"
 	"github.com/ramadani/go-api-skeleton/app/todo"
 
-	"github.com/ramadani/go-api-skeleton/app/todo/data"
+	"github.com/ramadani/go-api-skeleton/app/todo/resource"
 )
 
 type TodoUseCase struct {
 	rp todo.Repository
 }
 
-func (uc *TodoUseCase) All() []data.Todo {
-	return uc.rp.All()
+func (uc *TodoUseCase) All() []resource.Todo {
+	todos := uc.rp.All()
+
+	return resource.Collection(todos)
 }
 
-func (uc *TodoUseCase) Create(title, body string) data.Todo {
-	return uc.rp.Create(title, body)
+func (uc *TodoUseCase) Create(title, body string) resource.Todo {
+	todo := uc.rp.Create(title, body)
+
+	return resource.Item(todo)
 }
 
-func (uc *TodoUseCase) Find(id uint) data.Todo {
-	return uc.rp.Find(id)
+func (uc *TodoUseCase) Find(id uint) resource.Todo {
+	todo := uc.rp.Find(id)
+
+	return resource.Item(todo)
 }
 
-func (uc *TodoUseCase) Update(title, body string, id uint) data.Todo {
-	return uc.rp.Update(title, body, id)
+func (uc *TodoUseCase) Update(title, body string, id uint) resource.Todo {
+	todo := uc.rp.Update(title, body, id)
+
+	return resource.Item(todo)
 }
 
 func (uc *TodoUseCase) Delete(id uint) response.Message {
