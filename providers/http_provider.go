f65a612e0d84ae20b095d1870f2565775a925a6b
Ramadani <email.ramadani@gmail.com>
2018-09-03 12:15:17 +0700

    costume logger middleware and refactor server file

--- providers/http_provider.go
+++ providers/http_provider.go
@@ -4,23 +4,18 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-	"github.com/ramadani/go-api-skeleton/config"
 	"github.com/ramadani/go-api-skeleton/middleware"
 )
 
 // HTTPProvider contains the library of framework.
 type HTTPProvider struct {
-	e   *echo.Echo
-	cog *config.Config
-	md  *middleware.Middleware
+	e  *echo.Echo
+	md *middleware.Middleware
 }
 
 // Boot the http.
 func (pd *HTTPProvider) Boot() {
-	if pd.cog.Config.GetBool("debug") {
-		pd.e.Use(pd.md.Logger())
-	}
-
+	pd.e.Use(pd.md.Logger())
 	pd.e.Use(pd.md.Recover())
 
 	pd.e.GET("/", func(c echo.Context) error {
@@ -34,6 +29,6 @@ func (pd *HTTPProvider) Boot() {
 }
 
 // NewHTTP returns route.
-func NewHTTP(e *echo.Echo, cog *config.Config, md *middleware.Middleware) *HTTPProvider {
-	return &HTTPProvider{e, cog, md}
+func NewHTTP(e *echo.Echo, md *middleware.Middleware) *HTTPProvider {
+	return &HTTPProvider{e, md}
 }
--- server.go
+++ server.go
@@ -14,14 +14,14 @@ func main() {
 	e := echo.New()
 	cog := config.Init()
 	db := db.Init(cog)
-	md := middleware.Init()
+	md := middleware.Init(cog)
 	app := bootstrap.New(e, cog)
 
 	if cog.Config.GetBool("db.auto_migrate") {
 		app.AddBootable(providers.NewDbMigration(db))
 	}
 
-	app.AddBootable(providers.NewHTTP(e, cog, md))
+	app.AddBootable(providers.NewHTTP(e, md))
 	app.AddBootable(todoRestApi.New(e, db))
 
 	defer db.Close()
