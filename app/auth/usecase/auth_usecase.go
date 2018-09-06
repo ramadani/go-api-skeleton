cc6754b75e52fbee55759ba9e369c903fe57db7d
Ramadani <email.ramadani@gmail.com>
2018-09-07 06:51:57 +0700

    compare hash and password

--- app/auth/usecase/auth_usecase.go
+++ app/auth/usecase/auth_usecase.go
@@ -14,11 +14,19 @@ type AuthUseCase struct {
 func (uc *AuthUseCase) Attempt(email, password string) (string, error) {
 	user, uErr := uc.rp.FindByEmail(email)
 	if uErr != nil {
+		// TODO with err
 		return "", uErr
+	}
+
+	bErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if bErr != nil {
+		// TODO with err
+		return "", bErr
 	}
 
 	token, tErr := uc.jwt.GenerateToken(user)
 	if tErr != nil {
+		// TODO with err
 		return "", tErr
 	}
 
@@ -28,12 +36,14 @@ func (uc *AuthUseCase) Attempt(email, password string) (string, error) {
 func (uc *AuthUseCase) Register(name, email, password string) (bool, error) {
 	bytes, errHash := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if errHash != nil {
+		// TODO with err
 		return false, errHash
 	}
 
 	passwordHashed := string(bytes)
 	_, errCreate := uc.rp.Create(name, email, passwordHashed)
 	if errCreate != nil {
+		// TODO with err
 		return false, errCreate
 	}
 
