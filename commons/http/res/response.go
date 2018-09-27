6376632a239ff01b3d6ee1477130cf030b491d73
Ramadani <email.ramadani@gmail.com>
2018-09-28 06:53:14 +0700

    create response

---
+++ commons/http/res/response.go
@@ -0,0 +1,53 @@
+package res
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Response wrapper
+type Response struct {
+	w http.ResponseWriter
+}
+
+// ResponseData for json response data
+type responseData struct {
+	Data interface{} `json:"data"`
+}
+
+// ResponseError for json response success
+type responseError struct {
+	Message string `json:"message"`
+}
+
+// Data wrapper
+func Data(data interface{}) interface{} {
+	return responseData{data}
+}
+
+// Error wrapper
+func Error(data string) interface{} {
+	return responseError{data}
+}
+
+// JSON response
+func (res *Response) JSON(data interface{}, statusCode int) {
+	result, err := json.Marshal(Data(data))
+	if err != nil {
+		res.Fail(err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.w.Header().Set("Content-Type", "application/json")
+	res.w.WriteHeader(statusCode)
+	res.w.Write(result)
+}
+
+// Fail response with json format
+func (res *Response) Fail(msg string, statusCode int) {
+	result, _ := json.Marshal(Data(Error(msg)))
+
+	res.w.Header().Set("Content-Type", "application/json")
+	res.w.WriteHeader(statusCode)
+	res.w.Write(result)
+}
